ca667160d201412c0e9efa38e3c03b0ebb267f9b
Cephetir <silyichev@gmail.com>
2021-12-24 22:43:54 +0300

    fixed

--- src/main/java/wtf/cephetir/christmasx/mixins/MixinGuiButton.java
+++ src/main/java/wtf/cephetir/christmasx/mixins/MixinGuiButton.java
@@ -38,29 +38,27 @@ public abstract class MixinGuiButton {
     @Shadow
     public int height;
 
+    @Shadow public boolean visible;
+
     @Inject(method = "drawButton", at = @At("HEAD"), cancellable = true)
     public void drawButton(Minecraft mc, int mouseX, int mouseY, CallbackInfo cir) {
-        if (ChristmasConfig.toggleButtons) {
+        if (ChristmasConfig.toggleButtons && this.visible) {
             cir.cancel();
             FontRenderer fontrenderer = mc.fontRendererObj;
             GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
             this.hovered = mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
-            int i = this.hovered ? 1 : 0;
             GlStateManager.enableBlend();
             GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
             GlStateManager.blendFunc(770, 771);
-            int p = new Color(255, 255, 255, 94).getRGB();
 
-            if (this.hovered) {
-                p = new Color(208, 59, 59, 255).getRGB();
-            }
+            int p = new Color(255, 255, 255, 94).getRGB();
+            if (this.hovered) p = new Color(208, 59, 59, 255).getRGB();
+
             RoundedUtils.drawRoundedRect((float) this.xPosition, (float) this.yPosition, (float) this.xPosition + (float) this.width, (float) this.yPosition + (float) this.height, 6.0F, p);
             this.mouseDragged(mc, mouseX, mouseY);
             int j = new Color(243, 100, 100, 255).getRGB();
 
-            if (this.hovered) {
-                j = new Color(255, 255, 255, 255).getRGB();
-            }
+            if (this.hovered) j = new Color(255, 255, 255, 255).getRGB();
 
             ((Gui) (Object) this).drawCenteredString(fontrenderer, displayString, (int) (this.xPosition + this.width / 2 + 0.7f), this.yPosition + (this.height - 8) / 2, j);
         }
